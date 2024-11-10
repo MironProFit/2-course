1317787f882e5fa0d0d8c396021f9b3ccc19516f
MironProFit <mpf_3d@vk.com>
2024-11-10 13:12:20 +0500

    практика 1

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+// Задача 1
+// let fib = [0, 1];
+// for (let i = 2; i <= 10; i++) {
+//     fib[i] = fib[i - 1] + fib[i - 2]
+// }
+// console.log(`10 первых чисел Фибоначчи`, fib.slice(0, 10));
+
+// Задача 2
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+// console.log("Пуск!");
+
+
+// Задача 3
+// for (let i = 0; i <= 100; i + i) {
+//     function (params) {
+        
+//     }
+// }
